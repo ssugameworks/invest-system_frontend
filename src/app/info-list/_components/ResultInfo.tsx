ee1cb1db9a12e1bbb125b2295cf6eff8d84ac3d2
Kangnets <kangnets88@gmail.com>
2025-09-22 13:42:10 +0900

    feat: 디자인 싱크 반영

--- src/app/info-list/_components/ResultInfo.tsx
+++ src/app/info-list/_components/ResultInfo.tsx
@@ -1,13 +1,16 @@
 interface ResultInfoProp {
-  first_line: string;
-  second_line: string;
+  num: number;
 }
 
-export function ResultInfo({ first_line, second_line }: ResultInfoProp) {
+export function ResultInfo({ num }: ResultInfoProp) {
   return (
     <div className="font-pretendard flex w-full flex-col gap-[0.375rem] text-left text-[1.25rem] leading-[1.25rem] font-bold tracking-[-0.0256rem] text-black">
-      <div>{first_line}</div>
-      <div>{second_line}</div>
+      <div className="flex">
+        여기&nbsp;<p className="text-[#4097F9]">내 조건</p>에 딱 맞는
+      </div>
+      <div className="flex">
+        장학 공고&nbsp;<p className="text-[#4097F9]">{num}개</p>를 찾았어요!
+      </div>
     </div>
   );
 }
--- src/app/info-list/page.tsx
+++ src/app/info-list/page.tsx
@@ -4,10 +4,7 @@ import { ResultLists } from '@/app/info-list/_components/ResultList';
 export default function InfoListPage() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <ResultInfo
-        first_line="여기 내 조건에 딱 맞는"
-        second_line="장학 공고 251개를 찾았어요!"
-      />
+      <ResultInfo num={251} />
       <ResultLists
         title={'소득연계형 국가장학금'}
         recruitmentStatus={'recruiting'}
