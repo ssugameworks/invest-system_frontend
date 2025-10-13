5ffb24bc3ff70a4b9850b291cecf95a8e98a19fc
AndyH0ng <chili.tomat0@icloud.com>
2025-10-14 00:36:57 +0900

    feat: 반응형 breakpoint 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ import plugin from 'tailwindcss/plugin'
 const config: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       colors: {
         /* Background Colors */
