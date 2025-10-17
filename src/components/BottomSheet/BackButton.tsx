70b54c0a3c4384780e25880c65e2167a61db77be
AndyH0ng <chili.tomat0@icloud.com>
2025-10-17 14:18:55 +0900

    feat: svgr로 관리

--- src/components/BottomSheet/BackButton.tsx
+++ src/components/BottomSheet/BackButton.tsx
@@ -1,3 +1,5 @@
+import ChevronLeftIcon from '@/assets/icons/chevron-left.svg';
+
 interface BackButtonProps {
   onClick: () => void;
 }
@@ -5,21 +7,7 @@ interface BackButtonProps {
 export default function BackButton({ onClick }: BackButtonProps) {
   return (
     <button onClick={onClick} className="absolute left-5 top-[3.4rem] z-10">
-      <svg
-        width="12"
-        height="23"
-        viewBox="0 0 12 23"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M11 1.5L1 11.5L11 21.5"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      <ChevronLeftIcon />
     </button>
   );
 }
--- src/components/BottomSheet/CommentInput.tsx
+++ src/components/BottomSheet/CommentInput.tsx
@@ -1,3 +1,5 @@
+import SendIcon from '@/assets/icons/send.svg';
+
 interface CommentInputProps {
   value: string;
   onChange: (value: string) => void;
@@ -19,21 +21,7 @@ export default function CommentInput({
         className="w-full bg-transparent font-pretendard text-sm font-light text-white placeholder:text-[rgba(136,136,136,0.53)] focus:outline-none"
       />
       <button onClick={onSubmit} className="shrink-0">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M22 2L11 13M22 2L15 22L11 13M22 2L2 9L11 13"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <SendIcon />
       </button>
     </div>
   );
